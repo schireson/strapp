0cd8387706bfeb2cc59668f249c9fbf073d02c17
Gordon Fierce <gordonf@known.is>
2023-10-23 11:58:57 -0400

    fix: update typing, stop testing 3.7

--- src/strapp/http/request.py
+++ src/strapp/http/request.py
@@ -241,7 +241,7 @@ def managed_request(retries=6, max_time=60 * 5, base=2, factor=3, exceptions=())
         base=base,
         factor=factor,
     )
-    def request_fn(fn: Callable[P, R], *args: P.args, **kwargs: P.kwargs) -> R:  # type: ignore
+    def request_fn(fn: Callable[P, R], *args: P.args, **kwargs: P.kwargs) -> R:
         return fn(*args, **kwargs)
 
     try:
